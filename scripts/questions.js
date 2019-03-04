6656cbe1f188c2c30125de156104dca82a1e4fd7
avelar <fortunato.avelar@gmail.com>
2019-03-04 16:11:10 -0300

    added new questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -4,6 +4,10 @@ const myQuestions = [
         res:`Oi, tudo ótimo. Vamos falar sobre escalada? Faça uma pergunta`
     },
     {
+        req:`Beleza?`,
+        res:`Oi, tudo ótimo. Vamos falar sobre escalada? Faça uma pergunta`
+    },
+    {
         req:`Qual seu nome?`,
         res:`Meu nome é BelayBot`
     },
@@ -20,7 +24,7 @@ const myQuestions = [
         res:` Wow Alex um Dude awesome além de ter uma fundação que ajuda a levar energia solar para locais remotos com pessoas carentes, Alex é um alpinista norte americano conhecido por suas ascensões livres de grandes paredes, Free Solo. Tem uma descrição bem completa \ <a href='https://en.wikipedia.org/wiki/Alex_Honnold' target="_blank">\ aqui \ </a> olha.`
     },
     {
-        req:`Vai chover?`,
+        req:`Vai chover? Será que chove?`,
         res:`Humm... boa pergunta talvez meu amigão google saiba \ <a href='https://www.google.com/search?q=tempo' target="_blank">\ vamos conferir juntos aqui \ </a>`
     },
     {
@@ -32,11 +36,15 @@ const myQuestions = [
         res:`Agora são ${new Date().getHours()}:${new Date().getMinutes()}`
     },
     {
-        req:`O que é a vida?`,
-        res:`42`
+        req:`Qual o sentido da vida? Qual sentido do universo`,
+        res:`A resposta é 42`
     },
     {
         req:`O que é Reglet?`,
         res:`Reglet é uma saliência na rocha, tipo um degrau de ecada só que com 1,5cm até 2,5cm... bem pequenininho né! O Blogdaescalada tem um vocabulário legal sobre agarras olha lá: \<a href="http://blogdescalada.com/vocabulario-da-escalada-quais-sao-os-tipos-de-agarras-que-existem/" target="_blanck"> vocabulário tipos de agarra </a> `
+    },
+    {
+        req:`Escalada é um esporte olímpico?`,
+        res:`Ainda não mas vai estar nas olimpíadas de 2020`
     },
 ]
